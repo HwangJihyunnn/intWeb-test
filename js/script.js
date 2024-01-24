cfd5a7224c1ad493bf87a181fadc8feb1599cbab
hjh <hjh_0524@naver.com>
2024-01-24 16:42:30 +0900

    test

--- js/script.js
+++ js/script.js
@@ -129,4 +129,5 @@ var markerPosition  = new kakao.maps.LatLng(37.520282, 126.890205);
   // 마커가 지도 위에 표시되도록 설정합니다
   marker.setMap(map);
   // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
-  // marker.setMap(null);      
+  // marker.setMap(null);  
+
